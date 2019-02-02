ea8032db0516e0b896e39f534207d2a89f15c951
patrickdewitNL <info@patrickdewit.eu>
2019-02-02 21:11:24 +0100

    Update module.php

--- IPS-P1-Smartmeter/module.php
+++ IPS-P1-Smartmeter/module.php
@@ -40,20 +40,22 @@
 			parent::ApplyChanges();
 			
 			$this->SetTimerInterval("DataRemoval", $this->ReadPropertyInteger("DaysToKeep")*24*60*60*1000);
-
+			
 			// Set variables for smart meter data
 			// Get ObjectID for first archive
 			$archives = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}");
  
 			
-			AC_SetLoggingStatus($this->GetIDForIdent('consumptionT1'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('consumptionT2'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('currentConsumption'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('currentProduction'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('productionT1'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('productionT2'), $archives[0], true);
-			AC_SetLoggingStatus($this->GetIDForIdent('consumptionGas'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('consumptionT1'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('consumptionT2'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('currentConsumption'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('currentProduction'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('productionT1'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('productionT2'), $archives[0], true);
+			echo AC_SetLoggingStatus($this->GetIDForIdent('consumptionGas'), $archives[0], true);
 			
+		
+
 		}
 		
 		public function PurgeOldData()
